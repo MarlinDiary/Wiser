9b3f724dbf6d67b4773d5285615d5de7931b77c1
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-08 17:27:58 +1200

    新增 Dial 视图，包含基本的文本显示功能，作为 Wiser 应用的一部分。

---
+++ Wiser/Dial.swift
@@ -0,0 +1,18 @@
+//
+//  Dial.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/8.
+//
+
+import SwiftUI
+
+struct Dial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dial()
+}
