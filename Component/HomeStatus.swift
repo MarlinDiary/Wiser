736c14fc28828e7a1562f252135fa85631a52eab
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-08 18:14:26 +1200

    新增多个视图组件，包括 DialBackground、DialContent、HomeStatus、HomeStatusControl 和 LabelIndicator，增强 Wiser 应用的功能和视觉效果，同时支持状态绑定和预览功能。

---
+++ Component/HomeStatus.swift
@@ -0,0 +1,14 @@
+//
+//  LabelIndicatorStatus.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/8.
+//
+
+import Foundation
+
+public enum HomeStatus {
+    case home
+    case log
+    case count
+}
