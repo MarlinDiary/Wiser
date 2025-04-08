ecfcfb933c03ecfe5bae2356b8af35fa8794bc2d
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-08 18:07:58 +1200

    新增 DialContent 视图，包含基本的文本显示功能，作为 Wiser 应用的一部分。

---
+++ Wiser/DialContent.swift
@@ -0,0 +1,18 @@
+//
+//  DialContent.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/8.
+//
+
+import SwiftUI
+
+struct DialContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DialContent()
+}
