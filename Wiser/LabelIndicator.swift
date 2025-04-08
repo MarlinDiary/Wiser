77e1c53696b42d5c8d0d20a6fe75e1ee997b64da
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-08 16:35:23 +1200

    新增 LabelIndicator 视图及其状态枚举，添加灰色、绿色和橙色的颜色资源文件。

---
+++ Wiser/LabelIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  LabelIndicator.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/8.
+//
+
+import SwiftUI
+
+struct LabelIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabelIndicator()
+}
---
+++ Wiser/LabelIndicatorStatus.swift
@@ -0,0 +1,8 @@
+//
+//  LabelIndicatorStatus.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/8.
+//
+
+import Foundation
