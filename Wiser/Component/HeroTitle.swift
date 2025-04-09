5588045a535fe2ac7cff8646d853204b1050c9d9
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-09 15:09:53 +1200

    新增 HeroTitle 视图，支持根据状态展示时间或标题，优化 Wiser 应用的界面灵活性和用户体验。

--- Wiser/Component/HeroTitle.swift
+++ Wiser/Component/HeroTitle.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct HeroTitle: View {
+    var status: HeroTitleStatus = .normal
+    var hour: String = "09"
+    var minute: String = "05"
+    var title: String = ""
+    
+    init(hour: String, minute: String) {
+        self.status = .time
+        self.hour = hour
+        self.minute = minute
+    }
+    
+    init (title: String) {
+        self.status = .normal
+        self.title = title
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            switch status {
+            case .time:
+                HStack(alignment: .firstTextBaseline) {
+                    Text(hour)
+                    Text("HOUR")
+                        .font(.system(size: 20))
+                    Text(minute)
+                    Text("MIN")
+                        .font(.system(size: 20))
+                }
+            case .normal:
+                Text(title)
+            }
+        }
+        .font(.system(size: 36))
+        .fontWeight(.bold)
     }
 }
 
+enum HeroTitleStatus {
+    case time
+    case normal
+}
+
 #Preview {
-    HeroTitle()
+    HeroTitle(title: "Hello")
+}
+
+#Preview {
+    HeroTitle(hour: "09", minute: "05")
 }
