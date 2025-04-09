4c781baca18cfa2cb282f5488484fea83007402e
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-09 19:19:55 +1200

    新增 HomeButton 视图，支持根据 HomeStatus 状态动态展示不同按钮文本和样式，提升 Wiser 应用的交互体验。

---
+++ Wiser/Component/HomeButton.swift
@@ -0,0 +1,18 @@
+//
+//  HomeButton.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/9.
+//
+
+import SwiftUI
+
+struct HomeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeButton()
+}
