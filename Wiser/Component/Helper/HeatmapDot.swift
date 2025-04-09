5227defa41125a570e40b74f975e55e69609f259
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-09 21:54:12 +1200

    新增 DialBackground、DialScale 和 HeatmapDot 视图，分别实现拨号背景、刻度和热图点的展示，提升 Wiser 应用的视觉效果和交互体验。

---
+++ Wiser/Component/Helper/HeatmapDot.swift
@@ -0,0 +1,64 @@
+//
+//  HeatmapDot.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/9.
+//
+
+import SwiftUI
+
+struct HeatmapDot: View {
+    @State private var isAnimating = false
+    var status: HeatmapDotStatus = .blank
+    var body: some View {
+        Group {
+            switch status {
+            case .blank:
+                Circle()
+                
+            case .now:
+                Circle()
+                    .overlay(
+                        Circle()
+                            .stroke(Color("orange1"), lineWidth: isAnimating ? 1 : 0.6)
+                            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: isAnimating)
+                    )
+                    .onAppear {
+                        isAnimating = true
+                    }
+                
+            case .record:
+                Circle()
+                    .foregroundStyle(.black)
+            case .select:
+                Circle()
+                    .foregroundStyle(.orange1)
+            }
+        }
+        .frame(width: 9, height: 9)
+        .foregroundStyle(Color("gray3"))
+    }
+}
+
+enum HeatmapDotStatus {
+    case blank
+    case record
+    case select
+    case now
+}
+
+#Preview {
+    HeatmapDot(status: .blank)
+}
+
+#Preview {
+    HeatmapDot(status: .now)
+}
+
+#Preview {
+    HeatmapDot(status: .record)
+}
+
+#Preview {
+    HeatmapDot(status: .select)
+}
