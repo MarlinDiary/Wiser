d3574f88f2f0ce1bbbe70d4615be65f79b06ae00
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-09 15:40:34 +1200

    新增 HeatmapDot 视图，支持多种状态的展示和动画效果，提升 Wiser 应用的可视化体验。

--- Wiser/Component/HeatmapDot.swift
+++ Wiser/Component/HeatmapDot.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct HeatmapDot: View {
+    @State private var isAnimating = false
+    var status: HeatmapDotStatus = .blank
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            switch status {
+            case .blank:
+                Circle()
+                
+            case .now:
+                Circle()
+                    .overlay(
+                        Circle()
+                            .stroke(Color("orange1"), lineWidth: isAnimating ? 1.2 : 0.6)
+                            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: isAnimating)
+                    )
+                    .onAppear {
+                        isAnimating = true
+                    }
+                
+            case .record:
+                Circle()
+                    .foregroundStyle(.black)
+            case .select:
+                Circle()
+                    .foregroundStyle(.orange1)
+            }
+        }
+        .frame(width: 9, height: 9)
+        .foregroundStyle(Color("gray3"))
     }
 }
 
+enum HeatmapDotStatus {
+    case blank
+    case record
+    case select
+    case now
+}
+
 #Preview {
-    HeatmapDot()
+    HeatmapDot(status: .blank)
+}
+
+#Preview {
+    HeatmapDot(status: .now)
+}
+
+#Preview {
+    HeatmapDot(status: .record)
+}
+
+#Preview {
+    HeatmapDot(status: .select)
 }
