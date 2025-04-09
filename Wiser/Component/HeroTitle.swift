064579ed44d41787d52e789e87b1385c34114a97
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-09 15:09:49 +1200

    新增 HeroTitle 视图，支持时间和标题两种状态的展示，增强 Wiser 应用的界面功能和灵活性。

---
+++ Wiser/Component/HeroTitle.swift
@@ -0,0 +1,18 @@
+//
+//  HeroTitle.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/9.
+//
+
+import SwiftUI
+
+struct HeroTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroTitle()
+}
