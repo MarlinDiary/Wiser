46a0c70960bb406d821847a0df5968eacd562205
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-10 15:39:18 +1200

    新增 LabelItem、LabelAvatarBig 和 LabelView 组件，优化标签展示和头像视图，提升用户界面的视觉效果和交互体验。同时新增灰色调色板 gray5，丰富颜色选择。

---
+++ Wiser/Component/LabelItem.swift
@@ -0,0 +1,18 @@
+//
+//  LabelItem.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/10.
+//
+
+import SwiftUI
+
+struct LabelItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabelItem()
+}
---
+++ Wiser/View/LabelView.swift
@@ -0,0 +1,18 @@
+//
+//  LabelView.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/10.
+//
+
+import SwiftUI
+
+struct LabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabelView()
+}
