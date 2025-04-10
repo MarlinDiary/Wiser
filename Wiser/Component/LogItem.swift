d3141de0b215032c577f0639c05e1b6df95c2e0b
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-10 13:21:51 +1200

    更新 LogItem 组件，新增时间范围和持续时长的格式化显示功能，优化用户界面的信息展示，提升交互体验。

--- Wiser/Component/LogItem.swift
+++ Wiser/Component/LogItem.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct LogItem: View {
+    @Binding var timeLog: TimeLog
+    
+    private func formattedTimeRange() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        
+        let startTimeString = formatter.string(from: timeLog.startTime)
+        
+        if let endTime = timeLog.endTime {
+            let endTimeString = formatter.string(from: endTime)
+            return "\(startTimeString) - \(endTimeString)"
+        } else {
+            return "\(startTimeString) - Unfinished"
+        }
+    }
+    
+    private func calculateDurationInHours() -> String {
+        let endTime = timeLog.endTime ?? Date()
+        let duration = endTime.timeIntervalSince(timeLog.startTime)
+        let hours = duration / 3600.0
+        return String(format: "%.1f H", hours)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            LabelAvatar(label: timeLog.label!)
+                .padding(.trailing)
+            VStack(alignment: .leading, spacing: 6) {
+                Text(timeLog.label!.name)
+                    .fontWeight(.medium)
+                Text(formattedTimeRange())
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            Text(calculateDurationInHours())
+                .font(.title2)
+                .fontWeight(.medium)
+        }
+        .padding()
     }
 }
 
 #Preview {
-    LogItem()
+    LogItem(timeLog: .constant(TimeLog.exampleTimeLogs[1]))
 }
