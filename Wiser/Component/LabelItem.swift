7ddfd4d27e51ad09a967f663dde09bfdac64c11b
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-10 15:39:22 +1200

    更新 LabelItem 和 LabelAvatarBig 组件，新增标签绑定和状态管理，优化头像视图尺寸，提升用户界面的视觉效果和交互体验。同时，更新 Label 数据模型以支持标签的激活状态。

--- Wiser/Component/LabelItem.swift
+++ Wiser/Component/LabelItem.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct LabelItem: View {
+    @Binding var label: Label
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LabelAvatarBig(label: label)
+                .saturation(label.isActive ? 1 : 0)
+            Text(label.name)
+                .foregroundStyle(.gray5)
+                .fontWeight(.medium)
+                .font(.system(size: 13))
+        }
+        .opacity(label.isActive ? 1 : 0.5)
     }
 }
 
 #Preview {
-    LabelItem()
+    LabelItem(label: .constant(Label.exampleLabels[1]))
+}
+
+#Preview {
+    LabelItem(label: .constant(Label.exampleLabels[2]))
 }
--- Wiser/Data/Label.swift
+++ Wiser/Data/Label.swift
@@ -13,16 +13,18 @@ class Label {
     var id: UUID
     var name: String
     var icon: Icon
+    var isActive: Bool
     
-    init(name: String, icon: Icon) {
+    init(name: String, icon: Icon, isActive: Bool = true) {
         self.id = UUID()
         self.name = name
         self.icon = icon
+        self.isActive = isActive
     }
     
     static let exampleLabels: [Label] = [
         Label(name: "Dog", icon: .dog),
         Label(name: "Grass", icon: .grass),
-        Label(name: "Boxing", icon: .boxing)
+        Label(name: "Boxing", icon: .boxing, isActive: false)
     ]
 }
