ff3a9f0a3080ec9c271f915795e48843ad4b3aa1
MarlinDiary <cni586@aucklanduni.ac.nz>
2025-04-10 13:21:48 +1200

    新增 LogItem 组件，负责显示时间日志的详细信息，包括时间范围和持续时长，提升用户界面的信息展示和交互体验。

---
+++ Wiser/Component/LogItem.swift
@@ -0,0 +1,18 @@
+//
+//  LogItem.swift
+//  Wiser
+//
+//  Created by Drawix on 2025/4/10.
+//
+
+import SwiftUI
+
+struct LogItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogItem()
+}
